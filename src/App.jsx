365461dc900c2af37cfb99bade21ce25aa5f463a
mohit-2102 <mohit.mathangi@gmail.com>
2025-09-20 12:46:54 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Navbar />
-    <div className='bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] min-h-[85vh]'>
+    <div className='bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] min-h-[78.5vh]'>
     <Manager />
     </div>
     <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-slate-800 text-white text-center p-4 sm:p-6">
-            <div className="text-lg sm:text-xl font-bold">
+        <div className="fixed bottom-0 w-full bg-slate-800 text-white text-center p-4 sm:p-6">
+            {/* Logo */}
+            <div className="text-lg sm:text-xl font-bold flex justify-center gap-3 ">
+                <div className="flex justify-center gap-0 items-center">
+
                 <span className="text-green-700">&lt;</span>
                 Pass
                 <span className="text-green-700">OP/&gt;</span>
-            </div>
-            <div className="text-sm sm:text-base">Created with ❤️ with reference</div>
-        </div>
+                </div>
 
+            {/* Tagline */}
+            <div className="text-sm sm:text-base mt-0.5">
+                Created with ❤️ with reference
+            </div>
+            </div>
+        </div>
     )
 }
 
